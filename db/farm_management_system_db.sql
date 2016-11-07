9ef98cfd2d450ccf02502c66dafebd013c767edf
Moonsglw <mnsglw@gmail.com>
2016-11-07 12:50:05 +0300

    Fixed data types

--- db/farm_management_system_db.sql
+++ db/farm_management_system_db.sql
@@ -18,8 +18,8 @@
 CREATE TABLE Animal_Location
 (
   Location_ID INT NOT NULL,
-  Location_Name VARCHAR NOT NULL,
-  Location_Description VARCHAR NOT NULL,
+  Location_Name VARCHAR(255) NOT NULL,
+  Location_Description VARCHAR(255) NOT NULL,
   PRIMARY KEY (Location_ID)
 );
 
@@ -28,40 +28,42 @@ CREATE TABLE Animal
   Animal_ID INT NOT NULL,
   Date_Of_Birth DATE NOT NULL,
   Date_Of_Weaning DATE,
-  Breed_Of_Animal VARCHAR NOT NULL,
+  Breed_Of_Animal VARCHAR(255) NOT NULL,
+  Animal_Number VARCHAR(255) NOT NULL,
   Location_ID INT NOT NULL,
   PRIMARY KEY (Animal_ID),
-  FOREIGN KEY (Location_ID) REFERENCES Animal_Location(Location_ID)
+  FOREIGN KEY (Location_ID) REFERENCES Animal_Location(Location_ID),
+  UNIQUE (Animal_Number)
 );
 
 CREATE TABLE Attendant
 (
   Attendant_ID INT NOT NULL,
-  Full_Names VARCHAR NOT NULL,
+  Full_Names VARCHAR(255) NOT NULL,
   Date_Joined DATE NOT NULL,
-  Residence VARCHAR NOT NULL,
+  Residence VARCHAR(255) NOT NULL,
   Contact INT NOT NULL,
-  Next_Of_Kin_Names VARCHAR NOT NULL,
+  Next_Of_Kin_Names VARCHAR(255) NOT NULL,
   Next_Of_Kin_Contact INT NOT NULL,
-  Primary_Role INT NOT NULL,
-  Secondary_Role INT,
-  Qualifications VARCHAR NOT NULL,
-  Additional_Training VARCHAR,
-  Current_Salary INT NOT NULL,
+  Primary_Role VARCHAR(255) NOT NULL,
+  Secondary_Role VARCHAR(255),
+  Qualifications VARCHAR(255) NOT NULL,
+  Additional_Training VARCHAR(255),
+  Current_Salary FLOAT NOT NULL,
   PRIMARY KEY (Attendant_ID)
 );
 
 CREATE TABLE Animal_Feed_Consumption
 (
   Date_Of_Consumption DATE NOT NULL,
-  Feed_Composition VARCHAR NOT NULL,
+  Feed_Composition VARCHAR(255) NOT NULL,
   Meal_Weight FLOAT NOT NULL,
   Water_Consumed FLOAT NOT NULL,
+  AF_Consumption_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
-  Location_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
+  PRIMARY KEY (AF_Consumption_ID),
   FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID),
-  FOREIGN KEY (Location_ID) REFERENCES Animal_Location(Location_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
 );
 
@@ -69,39 +71,41 @@ CREATE TABLE Animal_Transfer_Schedule
 (
   Date_Of_Transfer DATE NOT NULL,
   New_Location_ID INT NOT NULL,
-  Purpose_Of_Transfer INT NOT NULL,
+  Purpose_Of_Transfer VARCHAR(255) NOT NULL,
+  AT_Schedule_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
-  Location_ID INT NOT NULL,
-  FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID),
-  FOREIGN KEY (Location_ID) REFERENCES Animal_Location(Location_ID)
+  PRIMARY KEY (AT_Schedule_ID),
+  FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID)
 );
 
 CREATE TABLE Animal_Weight_Tracker
 (
   Date_Of_Weighin DATE NOT NULL,
   Current_Weight INT NOT NULL,
+  AW_Tracker_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
-  Location_ID INT NOT NULL,
-  FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID),
-  FOREIGN KEY (Location_ID) REFERENCES Animal_Location(Location_ID)
+  PRIMARY KEY (AW_Tracker_ID),
+  FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID)
 );
 
 CREATE TABLE Animal_Death_Tracker
 (
   Date_Of_Death DATE NOT NULL,
-  Weight_At_Death INT NOT NULL,
-  Cause_Of_Death VARCHAR NOT NULL,
+  Weight_At_Death FLOAT NOT NULL,
+  Cause_Of_Death VARCHAR(255) NOT NULL,
+  AD_Tracker_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
+  PRIMARY KEY (AD_Tracker_ID),
   FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID)
 );
 
 CREATE TABLE Supplier
 (
   Supplier_ID INT NOT NULL,
-  Supplier_Name VARCHAR NOT NULL,
-  Item_Supplied INT NOT NULL,
+  Supplier_Name VARCHAR(255) NOT NULL,
+  Item_Supplied VARCHAR(255) NOT NULL,
   Supplier_Contact INT NOT NULL,
-  Supplier_Address INT NOT NULL,
+  Supplier_Address VARCHAR(255) NOT NULL,
   PRIMARY KEY (Supplier_ID)
 );
 
@@ -109,7 +113,7 @@ CREATE TABLE Feeds_Requisiton
 (
   Feed_Requisition_ID INT NOT NULL,
   Date_Of_Requisition DATE NOT NULL,
-  Item_Requisitioned VARCHAR NOT NULL,
+  Item_Requisitioned VARCHAR(255) NOT NULL,
   Quantity INT NOT NULL,
   Attendant_ID INT NOT NULL,
   PRIMARY KEY (Feed_Requisition_ID),
@@ -119,7 +123,7 @@ CREATE TABLE Feeds_Requisiton
 CREATE TABLE Feeds_Receipt_Attendant
 (
   Feed_Receipt_Att_ID INT NOT NULL,
-  Item_Received VARCHAR NOT NULL,
+  Item_Received VARCHAR(255) NOT NULL,
   Date_Of_Receipt DATE NOT NULL,
   Quantity INT NOT NULL,
   Attendant_ID INT NOT NULL,
@@ -131,9 +135,9 @@ CREATE TABLE Feeds_Return
 (
   Feeds_Return_ID INT NOT NULL,
   Date_Of_Return DATE NOT NULL,
-  Item_Returned VARCHAR NOT NULL,
+  Item_Returned VARCHAR(255) NOT NULL,
   Quantity INT NOT NULL,
-  Reason_Of_Return INT NOT NULL,
+  Reason_Of_Return VARCHAR(255) NOT NULL,
   Attendant_ID INT NOT NULL,
   PRIMARY KEY (Feeds_Return_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
@@ -143,7 +147,7 @@ CREATE TABLE Feeds_Purchase_Order
 (
   Feed_Purchase_Order_ID INT NOT NULL,
   Date_Of_Purchase_Order DATE NOT NULL,
-  Item_Purchased VARCHAR NOT NULL,
+  Item_Purchased VARCHAR(255) NOT NULL,
   Quantity INT NOT NULL,
   Supplier_ID INT NOT NULL,
   PRIMARY KEY (Feed_Purchase_Order_ID),
@@ -152,27 +156,29 @@ CREATE TABLE Feeds_Purchase_Order
 
 CREATE TABLE Pig_Sales
 (
-  Customer_Name VARCHAR NOT NULL,
+  Customer_Name VARCHAR(255) NOT NULL,
   Date_Of_Sale DATE NOT NULL,
   Price_Of_Animal FLOAT NOT NULL,
   Quantity INT NOT NULL,
+  Pig_Sales_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
+  PRIMARY KEY (Pig_Sales_ID),
   FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID)
 );
 
 CREATE TABLE Expenditure
 (
   Expenditure_ID INT NOT NULL,
-  Expenditure_Type VARCHAR NOT NULL,
-  Expenditure_Description VARCHAR NOT NULL,
+  Expenditure_Type VARCHAR(255) NOT NULL,
+  Expenditure_Description VARCHAR(255) NOT NULL,
   PRIMARY KEY (Expenditure_ID)
 );
 
 CREATE TABLE Expenditure_Voucher
 (
   Voucher_ID INT NOT NULL,
-  Date_Of_Sale INT NOT NULL,
-  Total_Amount_Paid INT NOT NULL,
+  Date_Of_Sale DATE NOT NULL,
+  Total_Amount_Paid FLOAT NOT NULL,
   Expenditure_ID INT NOT NULL,
   Supplier_ID INT,
   PRIMARY KEY (Voucher_ID),
@@ -183,17 +189,19 @@ CREATE TABLE Expenditure_Voucher
 CREATE TABLE Attendant_Activity_Log
 (
   Date_Of_Activity DATE NOT NULL,
-  Description_Of_Activity VARCHAR NOT NULL,
-  Verified_By INT NOT NULL,
+  Description_Of_Activity VARCHAR(255) NOT NULL,
+  Verified_By VARCHAR(255) NOT NULL,
+  Att_Activity_Log_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
+  PRIMARY KEY (Att_Activity_Log_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
 );
 
 CREATE TABLE Chicken_Storage
 (
   Cage_ID INT NOT NULL,
-  Cage_Number VARCHAR NOT NULL,
-  Cage_Description VARCHAR NOT NULL,
+  Cage_Number VARCHAR(255) NOT NULL,
+  Cage_Description VARCHAR(255) NOT NULL,
   PRIMARY KEY (Cage_ID)
 );
 
@@ -204,10 +212,12 @@ CREATE TABLE Chicken_Population_Tracker
   Num_Of_Transfers_Out INT,
   Num_Of_Deaths INT,
   Num_Of_Chicken INT NOT NULL,
-  Cage_ID INT NOT NULL,
+  CP_Tracker_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
-  FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID),
-  FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
+  Cage_ID INT NOT NULL,
+  PRIMARY KEY (CP_Tracker_ID),
+  FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
+  FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
 
 CREATE TABLE Egg_Collection_Tracker
@@ -217,9 +227,11 @@ CREATE TABLE Egg_Collection_Tracker
   Num_Of_Cocks INT NOT NULL,
   Num_Of_Eggs_Collected INT NOT NULL,
   Num_Of_Damaged_Eggs INT,
-  Verified_By VARCHAR NOT NULL,
+  Verified_By VARCHAR(255) NOT NULL,
+  EC_Tracker_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
+  PRIMARY KEY (EC_Tracker_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
   FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
@@ -227,11 +239,13 @@ CREATE TABLE Egg_Collection_Tracker
 CREATE TABLE Chicken_Feed_Consumption
 (
   Date_Of_Consumption DATE NOT NULL,
-  Feed_Composition VARCHAR NOT NULL,
-  Total_Weight INT NOT NULL,
-  Water_Provided INT NOT NULL,
+  Feed_Composition VARCHAR(255) NOT NULL,
+  Total_Weight FLOAT NOT NULL,
+  Water_Provided FLOAT NOT NULL,
+  CF_Consumption_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
+  PRIMARY KEY (CF_Consumption_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
   FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
@@ -243,8 +257,10 @@ CREATE TABLE Chicken_Transfer_Tracker
   Num_Of_Cocks_Transfered INT NOT NULL,
   Receiving_Cage_ID INT NOT NULL,
   Receiving_Attendant_ID INT NOT NULL,
+  CT_Tracker_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
+  PRIMARY KEY (CT_Tracker_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
   FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
@@ -254,14 +270,16 @@ CREATE TABLE Chicken_Medication_Tracker
   Date_Of_Medication DATE NOT NULL,
   Num_Of_Hens INT,
   Num_Of_Cocks INT,
-  Type_Of_Medication VARCHAR NOT NULL,
-  Dosage INT NOT NULL,
-  Purpose VARCHAR NOT NULL,
-  Administered_By VARCHAR NOT NULL,
-  Attendant_ID INT NOT NULL,
+  Type_Of_Medication VARCHAR(255) NOT NULL,
+  Dosage VARCHAR(255) NOT NULL,
+  Purpose VARCHAR(255) NOT NULL,
+  Administered_By VARCHAR(255) NOT NULL,
+  CM_Tracker_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
-  FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
-  FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
+  Attendant_ID INT NOT NULL,
+  PRIMARY KEY (CM_Tracker_ID),
+  FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID),
+  FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
 );
 
 CREATE TABLE Chicken_Sales
@@ -269,10 +287,12 @@ CREATE TABLE Chicken_Sales
   Date_Of_Sale DATE NOT NULL,
   Num_Of_Hens INT,
   Num_Of_Cocks INT,
-  Customer_Name VARCHAR NOT NULL,
-  Total_Amount INT NOT NULL,
+  Customer_Name VARCHAR(255) NOT NULL,
+  Total_Amount FLOAT NOT NULL,
+  Chicken_Sales_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
+  PRIMARY KEY (Chicken_Sales_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
   FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
@@ -281,9 +301,11 @@ CREATE TABLE Egg_Dispatch
 (
   Date_Of_Dispatch DATE NOT NULL,
   Num_Of_Trays INT NOT NULL,
-  Verified_By VARCHAR NOT NULL,
+  Verified_By VARCHAR(255) NOT NULL,
+  Egg_Dispatch_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
   Cage_ID INT NOT NULL,
+  PRIMARY KEY (Egg_Dispatch_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID),
   FOREIGN KEY (Cage_ID) REFERENCES Chicken_Storage(Cage_ID)
 );
@@ -291,12 +313,14 @@ CREATE TABLE Egg_Dispatch
 CREATE TABLE Animal_Medication_Tracker
 (
   Date_Of_Medication DATE NOT NULL,
-  Dosage VARCHAR NOT NULL,
-  Purpose INT NOT NULL,
-  Type_Of_Medication VARCHAR NOT NULL,
-  Administered_By VARCHAR NOT NULL,
+  Dosage VARCHAR(255) NOT NULL,
+  Purpose VARCHAR(255) NOT NULL,
+  Type_Of_Medication VARCHAR(255) NOT NULL,
+  Administered_By VARCHAR(255) NOT NULL,
+  AM_Tracker_ID INT NOT NULL,
   Animal_ID INT NOT NULL,
   Attendant_ID INT NOT NULL,
+  PRIMARY KEY (AM_Tracker_ID),
   FOREIGN KEY (Animal_ID) REFERENCES Animal(Animal_ID),
   FOREIGN KEY (Attendant_ID) REFERENCES Attendant(Attendant_ID)
 );
@@ -305,7 +329,7 @@ CREATE TABLE Feeds_Receipt_Supplier
 (
   Feed_Receipt_Sup_ID INT NOT NULL,
   Date_Of_Receipt DATE NOT NULL,
-  Item_Received VARCHAR NOT NULL,
+  Item_Received VARCHAR(255) NOT NULL,
   Quantity INT NOT NULL,
   Supplier_ID INT NOT NULL,
   PRIMARY KEY (Feed_Receipt_Sup_ID),
